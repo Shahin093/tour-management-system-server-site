02a764038889f61138c0aa42b9d6529444d8917c
Shidul Islam <sishahin093@gmail.com>
2022-10-04 11:07:27 +0600

    schema model ready

---
+++ model/Tours.js
@@ -0,0 +1,49 @@
+const { default: mongoose } = require("mongoose");
+
+
+
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        minLength: [4, "At least 6 characters"],
+        mixLength: [100, "must not be 100 characters upon"]
+    },
+    image: {
+        type: String,
+        required: [true, 'image is required']
+    },
+    location: {
+        lat: {
+            type: Number,
+            require: true,
+            default: '0.000'
+        },
+        lng: {
+            type: Number,
+            required: true,
+            default: '0.000'
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    price: {
+        type: Number,
+        min: [0, 'Price can not be less than0'],
+        required: true
+    },
+    views: {
+        type: Number,
+        min: [0, 'Views can not be less then 0'],
+        required: true,
+        default: 0
+    }
+});
+
+const Tour = mongoose.Model('Tour', tourSchema);
+
+module.exports = Tour;
+
+
